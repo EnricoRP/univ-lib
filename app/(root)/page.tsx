07cd8e00243655219ddeedd6f4c380d78a7de968
EnricoRP <email@enricorp.com>
2025-06-10 13:53:31 +0700

    settings up main layout on root

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import BookList from '@/components/BookList'
+import BookOverview from '@/components/BookOverview'
+import { sampleBooks } from '@/constants'
+import React from 'react'
+
+const Home = () => {
+  return <>
+  <BookOverview {...sampleBooks[0]} />
+
+  <BookList />
+  </>
+}
+
+export default Home
